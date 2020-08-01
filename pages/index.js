e2608d177debe8dec6512dd38fa9d58fab0a9e8f
cktang88 <arctangent20@yahoo.com>
2020-07-31 21:21:17 -0500

    Add game logic

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import styles from "../styles/Home.module.css";
 import Monopoly from "../assets/monopoly";
 import { useState } from "react";
 
+const pickRandElem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
 const DiceButton = () => {
   const [val, setVal] = useState(6);
   return (
     <button
       onClick={() => {
-        // lol not acc
-        setVal(Math.floor(Math.random() * 12) + 1);
+        setVal(
+          Math.floor(Math.random() * 6) + Math.floor(Math.random() * 6) + 1
+        );
         console.log("rolled", val);
       }}
     >
@@ -19,13 +22,13 @@ const DiceButton = () => {
 };
 
 const BuyButton = ({ cost, onClick }) => {
-  console.log(cost);
+  // console.log(cost);
   return <button onClick={onClick}>Buy for ${cost}</button>;
 };
 
 const ActionButton = ({ descript, action }) => {
-  console.log(descript);
-  return <button onClick={action}>Action: {descript}</button>;
+  // console.log(descript);
+  return <button onClick={action}>{descript}</button>;
 };
 
 export default function Home() {
@@ -44,6 +47,83 @@ export default function Home() {
   ]);
   let [turn, setTurn] = useState(0);
 
+  let curPlayerId = () => players[turn].id;
+  let addPlayerMoney = (id, amount) => {
+    newPlayers = [...players];
+    players[id].money += amount;
+    setPlayers(newPlayers);
+  };
+  let setPlayerPos = (id, pos) => {
+    newPlayers = [...players];
+    players[id].pos = pos;
+    setPlayers(newPlayers);
+  };
+
+  const specialActions = (p) => {
+    () => {
+      switch (p.id) {
+        case "go":
+          return () => addPlayerMoney(curPlayerId, 200);
+          break;
+        case "jail":
+          return () => setPlayerPos(curPlayerId, 10);
+          break;
+        case "chance":
+          return () => {
+            let card = pickRandElem(chance);
+          };
+          break;
+        case "freeparking":
+          return () => setPlayerPos(curPlayerId, 10);
+          break;
+        case "communitychest":
+          return () => {
+            let card = pickRandElem(communitychest);
+            if (card.tileid) {
+              setPlayerPos(
+                curPlayerId,
+                properties.filter((p) => p.id == card.tileid)[0].position
+              );
+            }
+            if (card.movenearest) {
+              //pass
+            }
+            if (card.move) {
+              //
+            }
+            if (card.addfunds) {
+              addPlayerMoney(curPlayerId, card.amount);
+            }
+            if (card.removefunds) {
+              addPlayerMoney(curPlayerId, -card.amount);
+            }
+            if (card.removefundstoplayers) {
+              //
+            }
+            if (card.propertycharges) {
+              //
+            }
+            if (card.jail) {
+              if (card.subaction === "goto") {
+              }
+              if (card.subaction === "getout") {
+                //
+              }
+            }
+            if (card.addfundsfromplayers) {
+            }
+          };
+          break;
+        case "gotojail":
+          return () => setPlayerPos(curPlayerId, 10);
+          break;
+        case "luxurytax":
+          return () => setPlayerPos(curPlayerId, 10);
+          break;
+      }
+    };
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -55,8 +135,16 @@ export default function Home() {
         <h1 className={styles.title}>Monopoly</h1>
 
         <p className={styles.description}>
-          Roll Dice
+          {/* Roll Dice */}
           <DiceButton />
+          <button
+            onClick={() => {
+              setTurn((turn) => (turn + 1) % players.length);
+              console.log(turn);
+            }}
+          >
+            Next Turn
+          </button>
         </p>
 
         <div className={styles.grid}>
@@ -66,39 +154,46 @@ export default function Home() {
             .map((p) => (
               <>
                 <div className={styles.card}>
-                  <p>{JSON.stringify(p)}</p>
+                  <p>{p.name}</p>
+
+                  <p>{p.mortgaged && "***Mortgaged"}</p>
+                  <p>
+                    {p.ownedby &&
+                      p.ownedby !== -1 &&
+                      `***owned by ${p.ownedby}`}
+                  </p>
+                  {p.buildings ? <p>Buildings: {p.buildings}</p> : null}
+
                   {p.price && (
-                    <BuyButton
-                      cost={p.price}
-                      onClick={() => {
-                        console.log("clicked");
-                      }}
+                    <>
+                      <p>rent = ${p.rent}</p>
+                      <BuyButton
+                        cost={p.price}
+                        onClick={() => {
+                          console.log("clicked");
+                        }}
+                      />
+                      {p.ownedby && (
+                        <ActionButton
+                          descript={"Pay Rent"}
+                          action={() => {
+                            //pass
+                          }}
+                        />
+                      )}
+                    </>
+                  )}
+                  {p.group === "Special" && (
+                    <ActionButton
+                      descript="special action"
+                      action={specialActions(p)}
                     />
                   )}
-                  <ActionButton
-                    descript={() => {
-                      //pass
-                    }}
-                    action={() => {
-                      //pass
-                    }}
-                  />
                 </div>
               </>
             ))}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
